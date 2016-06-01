5be2025b9fe4d8b09a21c4a4df7dbf168e9d79b0
oueve <oueve.ios@gmail.com>
2016-05-31 21:28:39 -0700

    Update 2.1
    Fixed:
    -whitespace on side of screen
    -changed some formatting on mobile
    -position of features subpage
    
    Known issues:
    -no transition when navbar is pressed
    -no phone image for mobile
    -footer is not attached to bottom of screen on some pages
    -some icons and/or titles may not be correct

--- mobilechange.js
+++ mobilechange.js
@@ -5,10 +5,14 @@ $(document).ready(function () {
         $("div.col-md-4").removeClass("border-desktop");
         $("div.col-md-4").addClass("border-mobile");
         $("div.col-md-6").removeClass("right-left-aligned");
+        $("div.col-md-6").append("<br>");
         $("div.col-md-3").removeClass("border-desktop");
         $("div.col-md-3").addClass("border-mobile");
         $(".desktop").remove();
     }
+    $('.navbar-collapse ul li a').click(function () {
+        $('.navbar-toggle:visible').click();
+    });
     if ($(document).width() < 989) {
         $("div.phone").removeClass("image");
         $("div.phone").addClass("drop-mobile");
@@ -16,5 +20,4 @@ $(document).ready(function () {
     if ($(".active") === true) {
         $("a.active").removeClass("a:hover");
     }
-    
 });
