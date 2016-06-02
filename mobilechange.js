64a601e1bc3804d2b42dc447682a7715b4096db7
oueve <oueve.ios@gmail.com>
2016-06-01 18:13:39 -0700

    Update 2.5
    Extended update;
    
    Fixed:
    -footer should be fixed to the bottom
    -added photos, descriptions
    -replaced <text> tags with proper conventions
    -added phone image to mobile; (some) support for tablets
    
    Known issues:
    -form fields that should be required are not required
    -formatting?

--- mobilechange.js
+++ mobilechange.js
@@ -1,14 +1,19 @@
 /*global $, jQuery, alert */
 $(document).ready(function () {
     "use strict";
-    if ($(document).width() < 500) {
+    if ($(document).width() < 1000 && $(document).width() > 500) {
         $("div.col-md-4").removeClass("border-desktop");
         $("div.col-md-4").addClass("border-mobile");
         $("div.col-md-6").removeClass("right-left-aligned");
         $("div.col-md-6").append("<br>");
         $("div.col-md-3").removeClass("border-desktop");
         $("div.col-md-3").addClass("border-mobile");
-        $(".desktop").remove();
+        $("div.desktop").addClass("padding");
+    }
+    if ($(document).width() < 500) {
+        $("div.desktop").remove();
+        $("div.phone").prepend('<div class="mobile">');
+        $("li.button-links").addClass("button-pad");
     }
     $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
