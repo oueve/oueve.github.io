47227298fd23ff7cc09f63dec3acee20687ae792
oueve <oueve.ios@gmail.com>
2016-06-01 18:30:45 -0700

    Update 2.5.2
    Fixed formatting issues in mobile

--- mobilechange.js
+++ mobilechange.js
@@ -1,7 +1,7 @@
 /*global $, jQuery, alert */
 $(document).ready(function () {
     "use strict";
-    if ($(document).width() < 1000 && $(document).width() > 500) {
+    if ($(document).width() < 1000) {
         $("div.col-md-4").removeClass("border-desktop");
         $("div.col-md-4").addClass("border-mobile");
         $("div.col-md-6").removeClass("right-left-aligned");
